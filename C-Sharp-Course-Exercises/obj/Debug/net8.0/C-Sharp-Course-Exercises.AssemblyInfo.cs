24b8b9a5f465158da889d71dfac500285fcd2d0e
BillStephens2022 <two4onebill@yahoo.com>
2024-02-27 14:16:33 -0500

    added examples for Strings

--- C-Sharp-Course-Exercises/obj/Debug/net8.0/C-Sharp-Course-Exercises.AssemblyInfo.cs
+++ C-Sharp-Course-Exercises/obj/Debug/net8.0/C-Sharp-Course-Exercises.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("C-Sharp-Course-Exercises")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+87bac2fc20373dc46240b9bf5f9c99f01984528a")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+cc0a30353ff77f591960f829adcdaef0de80a436")]
 [assembly: System.Reflection.AssemblyProductAttribute("C-Sharp-Course-Exercises")]
 [assembly: System.Reflection.AssemblyTitleAttribute("C-Sharp-Course-Exercises")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
