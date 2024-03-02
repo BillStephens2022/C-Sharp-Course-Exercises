b8fac10e0a99bc1b1d2ce0b25f146de3c35df990
BillStephens2022 <two4onebill@yahoo.com>
2024-03-02 06:37:23 -0500

    added an example of using an interface, specifically the IEquatable interface

---
+++ C-Sharp-Course-Exercises/08E-Inheritance5-Interfaces/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace C_Sharp_Course_Exercises._08E_Inheritance5_Interfaces
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Ticket t1 = new Ticket(10);
+            Ticket t2 = new Ticket(10);
+            Ticket t3 = new Ticket(6);
+            Console.WriteLine($"Does ticket 1 equal ticket 2? {t2.Equals(t1)}");
+            Console.WriteLine($"Does ticket 1 equal ticket 3? {t3.Equals(t1)}");
+        }
+    }
+}
---
+++ C-Sharp-Course-Exercises/08E-Inheritance5-Interfaces/Ticket.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace C_Sharp_Course_Exercises._08E_Inheritance5_Interfaces
+{
+    internal class Ticket : IEquatable<Ticket>
+    {
+        // property to store the duration of the ticket in hours
+        public int DurationInHours { get; set; }
+
+        // constructor
+        public Ticket(int durationInHours)
+        {
+            DurationInHours = durationInHours;
+        }
+
+        public bool Equals(Ticket other)
+        {
+            return this.DurationInHours == other.DurationInHours;
+        }
+    }
+}
