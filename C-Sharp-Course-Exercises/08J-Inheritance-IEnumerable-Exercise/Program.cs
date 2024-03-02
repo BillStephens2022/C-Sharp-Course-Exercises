59b88f0fe78a859d11c37a81e5952de52240c896
BillStephens2022 <two4onebill@yahoo.com>
2024-03-02 09:15:43 -0500

    completed coding exercise using another example of IEnumerable and iterating through a phonebook of contacts

---
+++ C-Sharp-Course-Exercises/08J-Inheritance-IEnumerable-Exercise/Program.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace C_Sharp_Course_Exercises._08J_Inheritance_IEnumerable_Exercise
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            PhoneBook MyPhoneBook = new PhoneBook();
+
+            foreach (Contact contact in MyPhoneBook)
+            {
+                contact.Call();
+            }
+        }
+
+        class Contact {
+            public string Name { get; set; }    
+            public string PhoneNumber { get; set; }
+
+            public Contact(string name, string phoneNumber)
+            {
+                Name = name;
+                PhoneNumber = phoneNumber;
+            }
+
+            public void Call()
+            {
+                // "Calling to X. Phone number is Y"
+                Console.WriteLine($"Calling to {Name}. Phone number is {PhoneNumber}");
+            }
+        }
+
+        class PhoneBook : IEnumerable<Contact>
+        {
+            public List<Contact> Contacts { get; set; }
+
+            public PhoneBook()
+            {
+
+                Contacts = new List<Contact>{
+                    new Contact("Andre", "435797087"),
+                    new Contact("Lisa", "435677087"),
+                    new Contact("Dine", "3457697087"),
+                    new Contact("Sofi", "4367697087")
+                };
+            }
+
+            IEnumerator<Contact> IEnumerable<Contact>.GetEnumerator()
+            {
+                return Contacts.GetEnumerator();
+            }
+
+            IEnumerator IEnumerable.GetEnumerator()
+            {
+                throw new NotImplementedException();
+            }
+        }
+    }
+}
--- C-Sharp-Course-Exercises/obj/Debug/net8.0/C-Sharp-Course-Exercises.AssemblyInfo.cs
+++ C-Sharp-Course-Exercises/obj/Debug/net8.0/C-Sharp-Course-Exercises.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("C-Sharp-Course-Exercises")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f029ba629e7a448da8a846f9d5d09512dc965df7")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e2730d283c17648b8abdda192d22ea98a0da747a")]
 [assembly: System.Reflection.AssemblyProductAttribute("C-Sharp-Course-Exercises")]
 [assembly: System.Reflection.AssemblyTitleAttribute("C-Sharp-Course-Exercises")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
