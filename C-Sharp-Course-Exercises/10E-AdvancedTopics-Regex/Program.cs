6a893cf7cc7e0014760a2f90b69d6f127132a1c0
BillStephens2022 <two4onebill@yahoo.com>
2024-03-04 16:54:17 -0500

    added example using Regex and regex cheatsheet txt file

---
+++ C-Sharp-Course-Exercises/10E-AdvancedTopics-Regex/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace C_Sharp_Course_Exercises._10E_AdvancedTopics_Regex
+{
+    internal class Program
+    {
+        static void Main(string[] args) 
+        {
+            string pattern = @"\d";
+            Regex regex = new Regex(pattern);
+
+            string text = "Hi there, my number is 1234321";
+
+            MatchCollection matchCollection = regex.Matches(text);
+
+            Console.WriteLine("{0} hits found: \n {1}", matchCollection.Count, text);
+
+            foreach(Match hit in matchCollection)
+            {
+                GroupCollection group = hit.Groups;
+                Console.WriteLine("{0} found at: {1}", group[0].Value, group[0].Index);
+            }
+        }
+    }
+}
--- C-Sharp-Course-Exercises/obj/Debug/net8.0/C-Sharp-Course-Exercises.AssemblyInfo.cs
+++ C-Sharp-Course-Exercises/obj/Debug/net8.0/C-Sharp-Course-Exercises.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("C-Sharp-Course-Exercises")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+7d0b202a32e802b8273e52326ba0698eded16284")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+773fa94e8c6d33b0df4ed0496aa07fb617c2d0d6")]
 [assembly: System.Reflection.AssemblyProductAttribute("C-Sharp-Course-Exercises")]
 [assembly: System.Reflection.AssemblyTitleAttribute("C-Sharp-Course-Exercises")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
